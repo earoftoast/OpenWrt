886c7fdf1d0842f3946d5b068240b74940c45c66
earoftoast <33612146+earoftoast@users.noreply.github.com>
2021-02-18 15:32:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,20 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add AdGuardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/extra-packages/adguardhome
+
+# Add Hello World
+git clone https://github.com/jerrykuku/lua-maxminddb package/extra-packages/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/extra-packages/helloworld/luci-app-vssr
+
+# Add OpenClash & compile po2lmo
+git clone -b master https://github.com/vernesong/OpenClash package/extra-packages/openclash
+pushd package/extra-packages/openclash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
